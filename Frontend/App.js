aa2c6784fe912a8aaf040a266caecfab2d60d771
Vishal1003 <vjha10032001@gmail.com>
2021-04-16 11:21:43 +0530

    Product card modified

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { View } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
 import ProductContainer from "./screens/Home/ProductContainer";
-
 
 export default function App() {
     return (
         <View>
-            <HomeScreen />
+            <ProductContainer />
         </View>
     );
 }
--- Frontend/components/Card/ProductCard.js
+++ Frontend/components/Card/ProductCard.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import StarRating from "./StarRating";
 
 const { width, height } = Dimensions.get("window");
 
 export default function ProductCard(props) {
     const { name, cost, category, imageUrl, seller } = props;
     return (
-        <View style={styles.container}>
+        <View style={styles.cardsWrapper}>
             <View style={styles.card}>
-                <Image style={styles.image} source={{ uri: imageUrl }} />
-                <View>
-                    <Text style={styles.title}>
-                        {name.length > 15 ? name.substring(0, 15 - 3) + "..." : name}
-                    </Text>
+                <View style={styles.cardImgWrapper}>
+                    <Image
+                        source={{
+                            uri: imageUrl
+                        }}
+                        resizeMode="cover"
+                        style={styles.cardImg}
+                    />
                 </View>
-                <View>
-                    <Text style={styles.price}>${cost}</Text>
+                <View style={styles.cardInfo}>
+                    <Text style={styles.cardTitle}>{name}</Text>
+                    <StarRating ratings={4} reviews={99} />
+                    <Text style={styles.cardDetails}>{seller}</Text>
+                    <Text style={(styles.cardDetails, { fontWeight: "bold" })}>
+                        Price : {cost} $
+                    </Text>
+                    <Text style={styles.cardDetails}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s 
+                    </Text>
                 </View>
             </View>
         </View>
@@ -23,41 +37,49 @@ export default function ProductCard(props) {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: width / 2 - 20,
-        height: height / 2 - 50,
-        padding: 10,
-        borderRadius: 10,
-        marginTop: 10,
-        marginBottom: 5,
-        marginLeft: 10,
-        alignItems: "center",
-        elevation: 8,
-        backgroundColor: "white"
-    },
-    image: {
-        width: width / 2 - 20 - 10,
-        height: width / 2 - 20 - 30,
-        borderRadius: 10,
-        backgroundColor: "transparent",
-        position: "absolute"
+    cardsWrapper: {
+        marginTop: 20,
+        width: "90%",
+        alignSelf: "center"
     },
     card: {
-        marginBottom: 10,
-        height: width / 2 - 20 - 90,
-        backgroundColor: "transparent",
-        width: width / 2 - 20 - 10
+        height: height / 2,
+        marginVertical: 10,
+        flexDirection: "column",
+        shadowColor: "#999",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5
     },
-    title: {
-        top: height / 4,
+    cardImgWrapper: {
+        flex: 1,
+        height: height / 2
+    },
+    cardImg: {
+        height: "100%",
+        width: "100%",
+        alignSelf: "center",
+        borderRadius: 8,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0
+    },
+    cardInfo: {
+        flex: 2,
+        padding: 10,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderBottomRightRadius: 8,
+        borderTopRightRadius: 8,
+        backgroundColor: "#fff"
+    },
+    cardTitle: {
         fontWeight: "bold",
-        fontSize: 14,
-        textAlign: "center"
+        fontSize: 18
     },
-    price: {
-        fontSize: 20,
-        color: "orange",
-        marginTop: 10
+    cardDetails: {
+        fontSize: 12,
+        color: "#444"
     }
 });
